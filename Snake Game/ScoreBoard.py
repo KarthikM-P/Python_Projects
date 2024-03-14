af77e4c4e02bcbae0bc991ef75c154cccdc74d28
Karthik mp <130955414+KarthikM-P@users.noreply.github.com>
2024-03-14 19:49:16 +0530

    Add files via upload
    There is a snake game which is developed using turtle module in python where the main class is the snake.py file the contain all the required functionality and responsible to run the code. To get output of this game, download all the related files and open main(snake.py) file and click run button

---
+++ Snake Game/ScoreBoard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+class Score(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update()
+
+    def update(self):
+        self.write(f"Score:{self.score}", align="center", font=("arial", 20, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over", align="center", font=("courier", 20, "normal"))
+    def score_update(self):
+        self.score += 1
+        self.clear()
+        self.update()
